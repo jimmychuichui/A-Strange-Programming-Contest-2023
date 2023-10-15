f7bd00f9bbf0f0fffc8d0cdab24386015ba091b2
kyc46 <kyc46@sfu.ca>
2023-10-15 11:15:29 -0700

    first push

---
+++ Bedtime/Bedtime.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    //2 var for 2 times
+    string time1, time2;
+
+    scanf("%s", time2);
+    scanf("%s", time1);
+
+
+    printf("%s\n", time1);
+    printf("%s\n", time2);
+    return 0;
+}
